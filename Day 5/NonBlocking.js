e48508477d5be49a9815293a4d5c8a7b03140d3c
edwin.cruz <edwin.cruz@EDWIC>
2022-07-28 10:18:15 -0600

    reestructuring the day 5 folder

--- Day 5/NonBlocking.js
+++ Day 5/NonBlocking.js
@@ -14,7 +14,6 @@ var timeStart = currentDate.getTime();
 //now, we are going to read a file
 var file = "resources/text.txt";
 var content = fs.readFileSync(file);
-console.log("" + content);
 
 
 /////////////////////////////////////////////////////////////////////////
@@ -27,5 +26,7 @@ var timeEnd = lastDate.getTime();
 /////////////////////////////////////////////////////////////////////////
 //define a chronometer, this mark the time of execution
 var elapsedTime = timeEnd-timeStart;
-console.log("\nTime of execution: " + elapsedTime + " ms");
+console.log("\nStart of execution: " + timeStart + " ms");
+console.log("End of execution: " + timeEnd + " ms");
+console.log("Time of execution: " + elapsedTime + " ms");
 /////////////////////////////////////////////////////////////////////////
