08e711a0619942ebc386abedb16034999934581e
EdwinCruz13 <edwin.cruz13@hotmail.com>
2022-07-28 09:09:20 -0600

    Adding files and resources the Day 5

---
+++ Day 5/NonBlocking.js
@@ -0,0 +1,31 @@
+//Name of the File is  : blocking-code.js
+
+//use the fs module
+var fs = require('fs');
+
+/////////////////////////////////////////////////////////////////////////
+//define a chronometer, this mark is the begining of the execution
+/////////////////////////////////////////////////////////////////////////
+var currentDate = new Date();
+var timeStart = currentDate.getTime();
+
+
+
+//now, we are going to read a file
+var file = "resources/text.txt";
+var content = fs.readFileSync(file);
+console.log("" + content);
+
+
+/////////////////////////////////////////////////////////////////////////
+//define a chronometer, this mark is the end of the execution
+/////////////////////////////////////////////////////////////////////////
+var lastDate = new Date();
+var timeEnd = lastDate.getTime();
+
+
+/////////////////////////////////////////////////////////////////////////
+//define a chronometer, this mark the time of execution
+var elapsedTime = timeEnd-timeStart;
+console.log("\nTime of execution: " + elapsedTime + " ms");
+/////////////////////////////////////////////////////////////////////////
